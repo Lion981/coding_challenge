6ec0679954b4d9163d100fe55c7b13133ce2fe03
= <=>
2023-10-12 17:57:02 +0100

    123

--- js_challenge.js
+++ js_challenge.js
@@ -1,4 +1,21 @@
-// write a for loop that prints your name 20 times
-for (let i=0; i<20; i++){
-    console.log("hi")
+// let i=3;
+// while(i<9){
+//     console.log("yes thank you")
+//     i++
+// 
+
+
+let friendANDfamily=["asad","asad2","asad3","Asad4","Asadd5","Asad6"]
+const today="friday"
+
+switch(today){
+    case "monday":
+        console.log("go to school from switch")
+        break;
+    case"tuesday":
+        console.log("go to work from switch ")
+        break;
+    default:
+        console.log("go to the mosque")  
+        break;  
 }
